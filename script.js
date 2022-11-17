679bad2937446e16de05abfa1ac2b2997a41d38a
theVanya-iam <48164993+theVanya-iam@users.noreply.github.com>
2022-11-17 10:56:03 -0500

    add update grid function and CSS

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-const grid_area = document.querySelector(".grid_area");
+const grid_area = document.querySelector(".grid_area"); 
+let mouse_in = false;
+let size = 16;
 
-let multiplier = 20; // the etch-a-sketch grid is 16 x 16;
+function createGrid(size){
+    grid_area.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid_area.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    let numDivs = size * size;
 
-function createGrid(multiplier){
-    
-    grid_area.style.gridTemplateColumns = `repeat(${multiplier}, 1fr)`;
-    grid_area.style.gridTemplateRows = `repeat(${multiplier}, 1fr)`;
-    // the above two lines determine the formulaic approach to making a grid
-    for (i=0; i < (multiplier * multiplier); i++){
+    for (i=0; i < numDivs; i++){
         const grid_Element = document.createElement("div");
         grid_Element.classList.add("grid_Element");
         grid_area.appendChild(grid_Element);
     }
-
-    let color_selection = "#00FF00";
-    // whatever the color user selects in the selection square;
-    
-    let mouse_in = false;
-    document.addEventListener("click", () => {
-        if (mouse_in === true){
-            this.style.backroundColor = 'red';
-        }
-    })
 };
 
+function resetGrid(){
+    
+}
+//function to reset grid to initial look
 
-// 2. Explore how to change color using mousemove and eventListeners
+function updateSize(){
+    let input = prompt("What will be size of the board?");
+    let message = document.querySelector('#message');
+    if (input===""){
+        message.innerHTML = "Please provide a number";
+    } else if (input <= 0 || input > 100){
+        message.innerHTML = "Please provide a number between 1 and 100";
+    } else {
+        message.innerHTML = "Now you can play!";
+        grid_area.innerHTML = "";
+        createGrid(input);
+    }
+//function to change the size of the grid based on users input
+}
 
-// 3. Explore how to adjust size of the grid using a mouse drag and horizontal scrollbar
-// 4. Figure out how to change color of the adjustments
-// 5. Add feature that allows 3-4 modes for changing colors/shade
-//      a) regular: selected color paints blocks
-//      b) rainbow: randomized colors paints each block differently
-//      c) shading: every block get 10% darker when mousemove occurs
-//      d) lighting: every block get 10% brigther when mousemove occurs
+grid_area.addEventListener("mousedown", () => {
+  mouse_in = true;
+  this.classList.add('colorSelection'); 
+});
+grid_area.addEventListener("mouseup", () => {
+    mouse_in = false;
+});
 
-window.onload = () => {
-    createGrid(multiplier);
-  }
+document.addEventListener("DOMContentLoaded", createGrid(size));
